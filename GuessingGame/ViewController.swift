04f9f012a9525b05db4df4ce2e263f8e2465cfe7
Bowler, Ryan <rbowler@lcs-mba2899.lcs.on.ca>
2018-01-19 09:55:27 -0500

    Broken Code

--- GuessingGame/ViewController.swift
+++ GuessingGame/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var numberGuessed: UITextField!
+    
+    // MARK: Properties (variables)
+    var game = GuessingGame()
+    
+    // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("The number to guess is:")
+        print(game.numberToGuess)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Actions
+    @IBAction func guessSubmitted(_ sender: Any) {
+        
+        // Print the number that was guessed
+        print(numberGuessed.text)
+        
+        // Use a guard statement to bind to a non-optional variable
+        guard let inputGiven = numberGuessed.text else {
+            // If the input was nil, stop and return (exit) the function
+            return
+        }
+        
+        // Use a guard statement to attempt to create an interger
+        guard let integerGiven = Int(inputGiven) else {
+            // If the input was text (e.g.: "five") we can't make an interger, so stop and return (exit) the function
+            return
+        }
+        
+        // Print the number that was guessed again
+        print(integerGiven)
+        
+        // The number to guess
+        print("The number to guess is:")
+        print(game.numberToGuess)
+        
+    }
+    
+    // MARK: Custom function(s)
+    
 }
-
